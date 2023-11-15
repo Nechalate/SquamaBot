253847f0005c1525bbb0e6427c719766d6bee79f
Nechalate <penpolpen@gmail.com>
2023-11-16 01:49:35 +0300

    Add color check on resolution 1920 1080

--- SquamaConsole/Pointer.cs
+++ SquamaConsole/Pointer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -16,6 +17,9 @@ namespace SquamaConsole
             public int pointY;
         }
 
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDc, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
         [DllImport("user32.dll")]
         static extern bool GetCursorPos(out POINT lpPoint);
 
@@ -28,15 +32,39 @@ namespace SquamaConsole
                     POINT point;
                     GetCursorPos(out point);
 
-                    Console.WriteLine($"Координаты мыши: X={point.pointX}, Y={point.pointY}");
+                    var color = GetColorAt(new System.Drawing.Point(1052, 899));
 
-                    Thread.Sleep(100);
+                    if (color.ToString() == "Color [A=255, R=255, G=0, B=0]")
+                    {
+                    Console.WriteLine("Рыбка клюет!");
+                    //Console.WriteLine($"Координаты мыши: X={point.pointX}, Y={point.pointY}");
+                    }
+                    //Console.WriteLine(color.ToString());
+
+                    Thread.Sleep(500);
                 }
             }
             catch (ThreadAbortException)
             {
 
             }
+        }
+        public static System.Drawing.Color GetColorAt(System.Drawing.Point location)
+        {
+            var screenPixel = new Bitmap(1, 1, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            using (var gdest = Graphics.FromImage(screenPixel))
+            {
+                using (var gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDc = gsrc.GetHdc();
+                    IntPtr hDc = gdest.GetHdc();
+                    BitBlt(hDc, 0, 0, 1, 1, hSrcDc, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
         }
     }
 }
