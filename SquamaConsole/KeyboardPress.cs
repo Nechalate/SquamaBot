3812d25b67764b339bba276704980baee799c69b
Nechalate <penpolpen@gmail.com>
2023-11-16 16:18:22 +0300

    Now the bot automatically casts a fishing rod after catching a fish

---
+++ SquamaConsole/KeyboardPress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquamaConsole
+{
+    internal class KeyboardPress
+    {
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,
+        string lpWindowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void RodPut()
+        {
+            IntPtr rageHandle = FindWindow("RAGE Multiplayer Launcher", "RAGE Multiplayer");
+            SetForegroundWindow(rageHandle);
+            SendKeys.SendWait("1");
+        }
+    }
+}
--- SquamaConsole/Pointer.cs
+++ SquamaConsole/Pointer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace SquamaConsole
 {
@@ -25,15 +26,25 @@ namespace SquamaConsole
                     GetCursorPos(out point);
 
                     var color = ColorTaker.GetColorAt(new System.Drawing.Point(1052, 899));
-
+                    var colorGrab = ColorTaker.GetColorAt(new System.Drawing.Point(661, 1019));
+                    
                     if (color.ToString() == "Color [A=255, R=255, G=0, B=0]")
                     {
                         MouseClick.Click(1920, 1080);
-                        Console.WriteLine("Рыбка клюет!");
+                        //Console.WriteLine("Рыбка клюет!");
                         //Console.WriteLine($"Координаты мыши: X={point.pointX}, Y={point.pointY}");
                     }
-                    //Console.WriteLine(color.ToString());
-                    Thread.Sleep(100);
+                    if (colorGrab.ToString() == "Color [A=255, R=148, G=248, B=7]")
+                    {
+                        Console.WriteLine("I see");
+                        Thread.Sleep(500);
+                        KeyboardPress.RodPut();
+                        Thread.Sleep(5000);
+                    }
+                        //Console.WriteLine(colorGrab.ToString());
+
+                        //Console.WriteLine($"Координаты мыши: X={point.pointX}, Y={point.pointY}");
+                        Thread.Sleep(100);
                 }
             }
             catch (ThreadAbortException)
